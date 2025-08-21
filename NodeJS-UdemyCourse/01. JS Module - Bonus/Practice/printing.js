8f56a944155a1009667a3a203ea7843e51e6e8ad
Siva Jallipalli <kumar.jallipalli@gmail.com>
2025-08-21 08:45:01 +0530

    Added JS Part 1 Files

---
+++ NodeJS-UdemyCourse/01. JS Module - Bonus/Practice/printing.js
@@ -0,0 +1,15 @@
+// This is similar to System.out.println() in JAVA
+console.log("Helllo JavaScript");
+
+// This is similar to System.out.print() in JAVA
+process.stdout.write("This won't create a new line");
+process.stdout.write("Hence, printed on the same line");
+
+console.log();
+// Prints data in Tabular form
+console.table({country: "India", city: "Lucknow"});
+
+// Printed as a Warning text [ Yellow Color ]
+console.warn("This is a Warning message");
+
+// Similarly, there are many more ways of printing in JS
